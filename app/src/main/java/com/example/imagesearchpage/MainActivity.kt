8b0a7df5893d10453496476fa49e25574118d62d
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-09-19 16:02:57 +0900

    [UI] 메인 화면 기초 구현 + 바텀네비게이션

--- app/src/main/java/com/example/imagesearchpage/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchpage/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.imagesearchpage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.imagesearchpage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNav()
     }
+
+    private fun initBottomNav() = binding.apply {
+        val searchFragment = SearchFragment()
+        val favoriteFragment = FavoriteFragment()
+
+        bottomNav.run {
+            setOnItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.btn_search -> changeFragment(searchFragment)
+                    R.id.btn_favorite -> changeFragment(favoriteFragment)
+                }
+                true
+            }
+            selectedItemId = R.id.btn_search
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentLayout.id, fragment)
+            .commit()
+    }
+
+
 }
